53a7617a49b18a1d8c282b77901592a0a2d640cf
Alex D Vallianos <alex@vallianos.(none)>
2011-06-08 09:46:33 +0400

    Initial Commit

---
+++ SendBox/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SendBox
+//
+//  Created by Alex D Vallianos on 08.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
